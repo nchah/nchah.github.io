53bbe92a20f10f3e22341a325b918a1cd45bff50
Niel C <nielchah@gmail.com>
2017-09-15 22:50:39 -0400

    Showing image

--- inf2190/js/graph.js
+++ inf2190/js/graph.js
@@ -109,13 +109,15 @@ function draw(checks) {
       .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
   var circle = svg.append("g").selectAll("circle")
       .data(force.nodes())
-    if (checkedValues.includes("showLogos") && function(d) { d.img }) {
-      circle.enter().append("image")
+    // if (checkedValues.includes("showLogos") && function(d) { d.img }) {
+      // circle.enter().append("image")
+      // .attr("xlink:href", function(d) { return d.img; })
+    // } else {
+      // circle.enter().append("circle")
+      // .attr("r", 6)
+    // }
+      .enter().append("image")
       .attr("xlink:href", function(d) { return d.img; })
-    } else {
-      circle.enter().append("circle")
-      .attr("r", 6)
-    }
     circle.attr("x", -16)
       .attr("y", -16)
       .attr("width", 32)
