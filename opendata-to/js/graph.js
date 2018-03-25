83670dd3655750745c20ffdd3d6cd941068f28c2
Niel C <nielchah@gmail.com>
2018-03-25 14:52:51 -0400

    Fixed tooltip for nodes

--- opendata-to/js/graph.js
+++ opendata-to/js/graph.js
@@ -7,11 +7,11 @@ var data_ttc = [
 // {source:'SEARCH: TTC', predicate:'dataset', target:'TTC Subway Delay Data', hover:' 21 MB, 1K rows'},
 
 {source:'TTC Bus Delay Data', predicate:'column', target:'TTC Bus Delay Data', hover:'6 MB, 5K rows'},
-// {source:'TTC Routes and Schedules - Routes', hover:'9 KB, 198 rows'},
-// {source:'TTC Routes and Schedules - Stops', hover:'733 KB, 10K rows'},
-// {source:'TTC Routes and Schedules - Trips', hover:'14 MB, 16K rows'},
-// {source:'TTC Streetcar Delay Data', hover:'1 MB, 14K rows'},
-// {source:'TTC Subway Delay Data', hover:' 21 MB, 1K rows'},
+{source:'TTC Routes and Schedules - Routes', predicate:'column', target:'TTC Routes and Schedules - Routes', hover:'9 KB, 198 rows'},
+{source:'TTC Routes and Schedules - Stops', predicate:'column', target:'TTC Routes and Schedules - Stops', hover:'733 KB, 10K rows'},
+{source:'TTC Routes and Schedules - Trips', predicate:'column', target:'TTC Routes and Schedules - Trips', hover:'14 MB, 16K rows'},
+{source:'TTC Streetcar Delay Data', predicate:'column', target:'TTC Streetcar Delay Data', hover:'1 MB, 14K rows'},
+{source:'TTC Subway Delay Data', predicate:'column', target:'TTC Subway Delay Data', hover:' 21 MB, 1K rows'},
 
 // Remove:
 // {source:'TTC Bus Delay Data', predicate:'datasize', target:'5.57 MB'},
